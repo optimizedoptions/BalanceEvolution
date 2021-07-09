29178f955a390b784d99e7349660abb3865cd93b
OptimizedOptions <87187378+optimizedoptions@users.noreply.github.com>
2021-07-09 17:37:47 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+
+setup(name='BalanceEvolution',version='0.1',
+      requires=['scipy','numpy','datetime'],
+      author='Optimized Options',
+      packages=['BalanceEvolution'])
+
+
