f8118bfb621aa6e39718450f4190616a1fcc8bfd
OptimizedOptions <87187378+optimizedoptions@users.noreply.github.com>
2021-07-16 16:59:20 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup,find_packages
+from setuptools import setup
 
 setup(name='BalanceEvolution',version='0.1',
-      requires=['scipy','numpy','datetime'],
+      requires=['scipy','numpy','matplotlib'],
       author='Optimized Options',
       packages=['BalanceEvolution'])
 
