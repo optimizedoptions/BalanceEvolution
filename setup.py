a17de8b2fbc8617b8730836aca3f7116e39c3ce5
OptimizedOptions <87187378+optimizedoptions@users.noreply.github.com>
2021-07-28 16:53:56 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='BalanceEvolution',
-      version='0.1',
-      description="It allows to perform Monte Carlo simulations of the evolution of a trading account based on the winning ratio of a trading strategy."
+      version='0.2',
+      description="It allows to perform Monte Carlo simulations of the evolution of a trading account based on the winning ratio of a trading strategy.",
       requires=['scipy','numpy','matplotlib'],
       author='Optimized Options',
       url="https://github.com/optimizedoptions/BalanceEvolution",
