c54ecb3832e80f1c9e1f99120fb00937faed016b
OptimizedOptions <87187378+optimizedoptions@users.noreply.github.com>
2021-08-04 18:53:30 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__),fname)).read()
+
 setup(name='BalanceEvolution',
-      version='0.2',
-      description="It allows to perform Monte Carlo simulations of the evolution of a trading account based on the winning ratio of a trading strategy.",
+      version='0.1.1',
+      description="Monte Carlo simulations of the trading account evolution based on the winning ratio and risk-reward relation of a trading plan.",
+      long_description_content_type='text/markdown',
+      long_description=read('Readme.MD'),
       requires=['scipy','numpy','matplotlib'],
       author='Optimized Options',
       url="https://github.com/optimizedoptions/BalanceEvolution",
       packages=['BalanceEvolution'])
+
+
